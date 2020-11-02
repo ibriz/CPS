f5caae76b9ceb9c43349bb322159a04d27a363ac
Tejasvi Raj Pant <tejpant@gmail.com>
2020-11-02 12:51:22 +0545

    Hiding vote box if already voted

--- Frontend/src/Redux/Sagas/Proposal/fetchProposalListWorker.js
+++ Frontend/src/Redux/Sagas/Proposal/fetchProposalListWorker.js
@@ -18,33 +18,33 @@ const proposalListStatusMapping = {
 
 function* fetchProposalListWorker({payload}) {
   try {
-//     const response = yield call(callKeyStoreWallet, {
-//       method: 'get_proposal_details',
-//       params: {_status: proposalListStatusMapping[payload.status],
-//         _wallet_address: payload.walletAddress,
-//         _end_index: `${(payload.pageNumber * 10) - 1}`,
-//       _start_index: `${(payload.pageNumber * 10) - 10}`,
+    const response = yield call(callKeyStoreWallet, {
+      method: 'get_proposal_details',
+      params: {_status: proposalListStatusMapping[payload.status],
+        _wallet_address: payload.walletAddress,
+        _end_index: `${(payload.pageNumber * 10) - 1}`,
+      _start_index: `${(payload.pageNumber * 10) - 10}`,
     
-//     }
-// });
-
-    const response = {
-      data: Array(10).fill(0).map((_, index) => (  {
-            _status:"_sponsor_pending",
-            _proposal_title :`${payload.status} ${payload.pageNumber}${index} New Proposal`,
-            _contributor_address : (index % 2 === 0) ? "hx177b10efe3018961a405cc7c20ee811c552831a5": "hxfd114a60eefa8e2c3de2d00dc5e41b1a0c7e8931",
-            budget : 10000,
-            _timestamp : 1600872145290985,
-            _ipfs_hash: "bafybeih63y3v5goi3if4e3yrwsjnc2lv7uiesui2cuamfvvgebki5ewo2e",
-              _ipfs_key: "proposal338e7daa-867e-4ea1-995b-b0e118b0036e",
-              approved_votes:"0x410d586a20a4c00000",
-              total_votes:"0xa2a15d09519be00000",
-              completed_percentage: "0x20"
-        }
-        )),
-        count: 143
-      
     }
+});
+
+    // const response = {
+    //   data: Array(10).fill(0).map((_, index) => (  {
+    //         _status:"_sponsor_pending",
+    //         _proposal_title :`${payload.status} ${payload.pageNumber}${index} New Proposal`,
+    //         _contributor_address : (index % 2 === 0) ? "hx177b10efe3018961a405cc7c20ee811c552831a5": "hxfd114a60eefa8e2c3de2d00dc5e41b1a0c7e8931",
+    //         budget : 10000,
+    //         _timestamp : 1600872145290985,
+    //         _ipfs_hash: "bafybeih63y3v5goi3if4e3yrwsjnc2lv7uiesui2cuamfvvgebki5ewo2e",
+    //           _ipfs_key: "proposal338e7daa-867e-4ea1-995b-b0e118b0036e",
+    //           approved_votes:"0x410d586a20a4c00000",
+    //           total_votes:"0xa2a15d09519be00000",
+    //           completed_percentage: "0x20"
+    //     }
+    //     )),
+    //     count: 143
+      
+    // }
     yield put(fetchProposalListSuccess(
       {
         response,
