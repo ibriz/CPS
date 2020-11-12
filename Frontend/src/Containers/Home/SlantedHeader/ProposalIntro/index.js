b8974c120423612ca54bc3575dab42b5290821d3
Tejasvi Raj Pant <tejpant@gmail.com>
2020-11-12 22:33:14 +0545

    Changed Proposal Intro

--- Frontend/src/Containers/Home/SlantedHeader/ProposalIntro/index.js
+++ Frontend/src/Containers/Home/SlantedHeader/ProposalIntro/index.js
@@ -4,7 +4,7 @@ import styles from './ProposalInfo.module.css';
 const ProposalIntro = () => {
     return (
         <div className = {styles.proposalIntro}>
-            <span style = {{fontWeight: 600}}>Welcome to the Contribution Proposal System.</span> <span> The CPS is part of the ICON Republic DAO governance. The CPS manages funds from block rewards through grants to worthy projects. Learn how to submit a proposal and get paid to contribute to the ICON DAO with work on infrastructure, development, or community activities</span>
+            <span style = {{fontWeight: 600}}>Welcome to the Contribution Proposal System.</span> <span> The CPS is part of the ICON Republic DAO governance. The CPS manages funds from block rewards through grants to worthy projects. Learn how to submit a proposal and get paid to contribute to the ICON DAO by working on infrastructure, development, or community activities</span>
         </div>
     )
 }
