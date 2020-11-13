ab51712d6b3a2583b2bca762a25c528c3279b02e
Tejasvi Raj Pant <tejpant@gmail.com>
2020-11-13 14:45:51 +0545

    Added InfoCard for Dashboard

---
+++ Frontend/src/Containers/Dashboard/InfoCard/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Row, Col, Card } from 'react-bootstrap';
+import styles from './InfoCard.module.scss';
+
+const InfoCard = ({bg = "secondary", title, value}) => {
+    return (
+        <Card bg = {bg} text={(bg === 'light' || bg === 'warning') ? 'dark' : 'white'} className = {styles.card} style = {{height: 'auto', minHeight: '100%'}}
+        >
+            <Card.Body className = {styles.cardBody}>
+                <Card.Title style = {{textAlign: 'center', fontSize: '30px'}}>{value}</Card.Title>
+                <Card.Text style = {{fontSize: '18px', textAlign: 'center'}}>
+                    {title}
+                 </Card.Text>
+
+            </Card.Body>
+        </Card>
+    )
+}
+
+export default InfoCard;
