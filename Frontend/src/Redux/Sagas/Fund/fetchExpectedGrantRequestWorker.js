d719c9344d0d73c1d9f9881b43e2fe7cbafe8a84
Tejasvi Raj Pant <tejpant@gmail.com>
2020-11-23 14:24:27 +0545

    Added wallet address param to fetch expected grant request

--- Frontend/src/Redux/Sagas/Fund/fetchExpectedGrantRequestWorker.js
+++ Frontend/src/Redux/Sagas/Fund/fetchExpectedGrantRequestWorker.js
@@ -1,18 +1,25 @@
-import { put, call} from 'redux-saga/effects';
+import { put, call, select} from 'redux-saga/effects';
 import {fetchExpectedGrantSuccess, fetchExpectedGrantFailure} from '../../Reducers/fundSlice';
 import {callKeyStoreWallet} from '../../ICON/utils';
 
 function* fetchCPFScoreAddressWorker({payload}) {
   try {
 
-//     const response = yield call(callKeyStoreWallet, {
-//         method: 'get_projected_fund'
-// });
+    const getAddress = (state) => state.account.address
+    const walletAddress = yield select(getAddress);
 
-const response = {
-    total_amount: '0x100',
-    sponsor_bond: '0x1000'
-}
+    const response = yield call(callKeyStoreWallet, {
+        method: 'get_projected_fund',
+        params: {
+          _wallet_address: walletAddress,
+  
+        }
+});
+
+// const response = {
+//     total_amount: '0x100',
+//     sponsor_bond: '0x1000'
+// }
 
     yield put(fetchExpectedGrantSuccess({
         response
