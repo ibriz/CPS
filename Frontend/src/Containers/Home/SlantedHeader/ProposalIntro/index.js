336c668bc2025d5aa192cb4665550842c79a2685
Tejasvi Raj Pant <tejpant@gmail.com>
2020-11-26 13:21:44 +0545

    Added icon forum link and changed sidebar icons

--- Frontend/src/Containers/Home/SlantedHeader/ProposalIntro/index.js
+++ Frontend/src/Containers/Home/SlantedHeader/ProposalIntro/index.js
@@ -5,7 +5,7 @@ const ProposalIntro = () => {
     return (
         <div className = {styles.proposalIntro}>
             <span style = {{fontWeight: 600}}>Welcome to the Contribution Proposal System.</span> <div style = {{marginTop: '15px'}}> The CPS is part of the ICON Republic DAO governance. The CPS manages funds from block rewards through grants to worthy projects. Learn how to submit a proposal and get paid to contribute to the ICON DAO by working on infrastructure, development, or community activities</div>
-            <div style = {{marginTop: '15px'}}>Link to the ICON Forum- <a href = "https://forum.icon.community/" target = "_blank" style = {{color: 'white', textDecoration: 'underline'}}>ICON Forum</a></div>
+            <div style = {{marginTop: '15px'}}>Discuss proposal ideas on the ICON Forum- <a href = "https://forum.icon.community/" target = "_blank" style = {{color: 'white', textDecoration: 'underline'}}>ICON Forum</a></div>
         </div>
     )
 }
