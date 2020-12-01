11003b3a48128d84824753e257d07211c9d9fcaf
Tejasvi Raj Pant <tejpant@gmail.com>
2020-12-01 16:37:56 +0545

    Made UpperCard Responsive

--- Frontend/src/Containers/Proposals/UpperCard/index.js
+++ Frontend/src/Containers/Proposals/UpperCard/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import styles from './UpperCard.module.scss';
-import { Row, Col, Card, Button } from 'react-bootstrap';
+import { Row, Col, Card, Button, Container } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import useTimer from 'Hooks/useTimer';
-import {updatePeriod} from 'Redux/Reducers/periodSlice';
+import { updatePeriod } from 'Redux/Reducers/periodSlice';
 import ConfirmationModal from 'Components/UI/ConfirmationModal';
 
 const UpperCard = ({ numberOfSubmittedProposals, updatePeriod, sponsorRequest, voting, isPrep, isRegistered, projectAmounts }) => {
@@ -20,13 +20,13 @@ const UpperCard = ({ numberOfSubmittedProposals, updatePeriod, sponsorRequest, v
     }
 
     if (remainingTimeSecond === 0) {
-        button = <Button variant="primary" className={styles.createProposalButton} onClick = {onClickUpdatePeriod}>UPDATE PERIOD </Button>
+        button = <Button variant="primary" className={styles.createProposalButton} onClick={onClickUpdatePeriod}>UPDATE PERIOD </Button>
         text = <span className={styles.proposalNumber}>Click button to trigger {period === 'APPLICATION' ? 'Voting' : 'Application'} Period</span>
 
     } else if (sponsorRequest || voting || (isPrep && isRegistered)) {
         text = null;
         button = null;
-        
+
     } else if (period === 'APPLICATION') {
         button = <Link to="/newProposal">
 
@@ -43,40 +43,71 @@ const UpperCard = ({ numberOfSubmittedProposals, updatePeriod, sponsorRequest, v
 
     }
 
+    const remainingTimeText = <>
+        {
+            !voting && !sponsorRequest &&
+            <span className={styles.proposalTitle}><b>{(period === 'VOTING') ? 'PROPOSAL SUBMISSIONS OPENS IN' : 'PROPOSAL SUBMISSION DEADLINE IN'}</b></span>
+
+        }
+
+        {
+            (sponsorRequest) &&
+            <span className={styles.proposalTitle}><b>{(period === 'VOTING') ? 'APPLICATION PERIOD STARTS IN' : 'APPLICATION PERIOD ENDS IN'}</b></span>
+
+        }
+
+        {
+            (voting) &&
+            <span className={styles.proposalTitle}><b>{(period === 'VOTING') ? 'VOTING PERIOD ENDS IN' : 'VOTING PERIOD STARTS IN'}</b></span>
+
+        }
+    </>
+
+    const remainingTimeValue = <span className={styles.proposalTitle}><b>{remainingTime.day}</b> DAYS <b>{remainingTime.hour}</b> HOURS <b>{remainingTime.minute}</b> MINUTES <b>{remainingTime.second}</b> SECONDS</span>;
+
+
 
     return (
         <Row>
             <Col>
                 <Card className={styles.upperCard}>
                     <Card.Body>
-                        {/* <Row>
-                            <Col lg="10">
-                                <Row>
-                                    <span className={styles.proposalTitle}><b>PROPOSAL SUBMISSIONS OPEN</b></span>
-                                </Row>
 
-                                <Row>
-                                    <span className={styles.proposalTitle}><b>12</b> DAYS <b>14</b> HOURS <b>11</b> MINUTES 20 SECONDS</span>
-                                </Row>
+                        <Container className = {styles.desktopContainer}>
+                            <Row style = {{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+                                {remainingTimeText}
+                                {text}
+
+                            </Row>
+
+                            <Row style = {{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+                                {remainingTimeValue}
+                                {button}
+                            </Row>
+
+                        </Container>
+
+                        <Row  className = {styles.mobileContainer}>
+                            <Col xl="6" style={{ alignItems: 'flex-start' }}>
+
+                                {remainingTimeText}
+                                {remainingTimeValue}
+
+
 
                             </Col>
 
-                            <Col lg="6">
-                                <Row>
-                                    <span className={styles.proposalNumber}>{numberOfSubmittedProposals} Proposals submitted</span>
-                                </Row>
-
-                                <Row>
-                                    <Link to="/newProposal">
-                                        <Button variant="info" className={styles.createProposalButton}>CREATE NEW PROPOSAL</Button>
-                                    </Link>                            </Row>
+                            <Col xl="6" style={{ alignItems: 'flex-start', marginTop: '10px' }}>
+                                {text}
+                                {
+                                    button
+                                }
 
                             </Col>
+                        </Row>
 
-                        </Row> */}
 
-
-                        <Row className={styles.upperCardRow}>
+                        {/* <Row className={styles.upperCardRow}>
                             {
                                 !voting && !sponsorRequest &&
                                 <span className={styles.proposalTitle}><b>{(period === 'VOTING') ? 'PROPOSAL SUBMISSIONS OPENS IN' : 'PROPOSAL SUBMISSION DEADLINE IN'}</b></span>
@@ -103,11 +134,11 @@ const UpperCard = ({ numberOfSubmittedProposals, updatePeriod, sponsorRequest, v
                                 button
                             }
 
-                        </Row>
+                        </Row> */}
                     </Card.Body>
                 </Card>
             </Col>
-            
+
             <ConfirmationModal
                 show={periodConfirmationShow}
                 onHide={() => setPeriodConfirmationShow(false)}
@@ -115,10 +146,10 @@ const UpperCard = ({ numberOfSubmittedProposals, updatePeriod, sponsorRequest, v
                 onConfirm={() => {
                     updatePeriod();
                 }} >
-                {                 
-                        <>
-                            <div>Are you sure you want to trigger period update?</div>
-                        </> 
+                {
+                    <>
+                        <div>Are you sure you want to trigger period update?</div>
+                    </>
                 }
 
             </ConfirmationModal>
