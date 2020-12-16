965cfaec7aa2ff1712ae831227800835c7748bfa
bibek54 <bibekkoirala54@gmail.com>
2020-12-16 20:12:15 +0545

    added time for signatures

--- lambda/authorizer/index.js
+++ lambda/authorizer/index.js
@@ -2,6 +2,8 @@ const secp = require('secp256k1');
 var ethUtil = require('ethereumjs-util');
 var sha3_256 = require('js-sha3').sha3_256;
 
+const MIN = 60 * 1000;
+
 function validateSignature(signature, address, payload) {
     const signatureArray = Buffer.from(signature, 'base64');
 
@@ -23,6 +25,12 @@ function validateSignature(signature, address, payload) {
     const decodedAddress = 'hx' + sha3_256(pubKey).slice(-40);
 
     return address === decodedAddress;
+}
+
+function isSignatureRecent(payload) {
+    const timestamp = payload.slice(payload.length - 13);
+
+    return (Date.now() - parseInt('0x' + timestamp)) < 5 * MIN;
 }
 
 function buildAllowAllPolicy(event) {
@@ -48,8 +56,9 @@ exports.handler = function (event, context, callback) {
 
     const { signature, address, payload } = event.headers;
 
-    if (!(signature && address && payload)) return callback('Unauthorized');
-    if (!validateSignature(signature, address, payload)) return callback('Unauthorized');
+    if (!(signature && address && payload)) return callback('Signature required');
+    if (!validateSignature(signature, address, payload)) return callback('Mismatched signature');
+    if (!isSignatureRecent(payload)) return callback('Signature expired');
 
     const authResponse = buildAllowAllPolicy(event);
 
