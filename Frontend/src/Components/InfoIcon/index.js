bac9492a4111d2ec584a53eb51935895ce07a2c4
Tejasvi Raj Pant <tejpant@gmail.com>
2020-12-17 22:37:42 +0545

    Changed InfoIcon and created Popup component

--- Frontend/src/Components/InfoIcon/index.js
+++ Frontend/src/Components/InfoIcon/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { AiFillDelete, AiFillQuestionCircle } from 'react-icons/ai';
-import {OverlayTrigger, Tooltip} from 'react-bootstrap'
+import { OverlayTrigger, Tooltip, Popover } from 'react-bootstrap'
 
-  
-const InfoIcon = ({description}) => {
+
+const InfoIcon = ({ description }) => {
     return (
-        <OverlayTrigger
-        placement="right"
-        delay={{ show: 250, hide: 400 }}
-        style = {{width: '1000px'}}
-        overlay = {<Tooltip style = {{width: '1000px'}} >{description}</Tooltip>}
-      >
-            <AiFillQuestionCircle style = {{marginLeft: '1px', cursor: 'help', fontSize: '1.1rem', color: '#1AAABA'}} variant = "info"/>
-    </OverlayTrigger>
+        <OverlayTrigger trigger="hover" placement="right"
+            overlay={
+                <Popover id="popover-basic" >
+                    <Popover.Content>
+                        <span cl>
+                            {description}
+                        </span>
+                    </Popover.Content>
+                </Popover>
+            }
+        >
+
+            <AiFillQuestionCircle style={{ marginLeft: '1px', cursor: 'help', fontSize: '1.1rem', color: '#1AAABA' }} variant="info" />
+        </OverlayTrigger>
 
     )
 }
