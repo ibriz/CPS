5e469cd88722a8a811bdc3edaf245d3137d325f7
Tejasvi Raj Pant <tejpant@gmail.com>
2020-12-22 19:11:16 +0545

    Created routes for unsubscribe page

---
+++ Frontend/src/Containers/UnsubscribePage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../Components/Header';
+import {Container} from 'react-bootstrap';
+
+const UnsubscribePage = () => {
+    return (
+        <Container>
+            unsubscribe
+        </Container>
+    );
+}
+
+export default UnsubscribePage;
