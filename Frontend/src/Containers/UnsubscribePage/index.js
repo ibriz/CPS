8a3e6be2a487e498ac5e703286051ff2abd74acc
Tejasvi Raj Pant <tejpant@gmail.com>
2020-12-22 21:44:51 +0545

    Started un-subscribe page

--- Frontend/src/Containers/UnsubscribePage/index.js
+++ Frontend/src/Containers/UnsubscribePage/index.js
@@ -1,13 +1,97 @@
 import React from 'react';
 import Header from '../../Components/Header';
-import {Container} from 'react-bootstrap';
+import {Container, Button} from 'react-bootstrap';
+import useQuery from 'Hooks/useQuery';
+import styles from './unsubscribePage.module.scss';
+import Footer from 'Components/Footer';
+import ConfirmationModal from 'Components/UI/ConfirmationModal';
+import {connect} from 'react-redux';
+import store from 'Redux/Store';
+import {NotificationManager} from 'react-notifications';
 
-const UnsubscribePage = () => {
+const UnsubscribePage = ({hasAddress}) => {
+    let query = useQuery();
+    let walletAddress = query.get("address");
+
+    let [unsubscribeConfirmationShow, setUnsubscribeConfirmationShow] = React.useState(false);
+
+    const customEvent = new CustomEvent('ICONEX_RELAY_REQUEST', {detail: { 
+        type: 'REQUEST_HAS_ADDRESS',
+        payload: `${walletAddress}`
+    }});
+
+
+    const checkHasAddressPromise = () => {
+
+        return new Promise((resolve, reject) => {
+            
+            const interFunction = () => {
+            const hasAddress = store.getState().account.hasAddress;
+              if (hasAddress !== undefined && hasAddress !== null) {
+                  
+                clearInterval(interFunction);
+                resolve({
+                    hasAddress
+                });
+                return;
+                  }
+              
+            }
+      
+            setInterval(interFunction, 100)
+        })
+    }
+
+    const onClickOptOut = async () => {
+        window.dispatchEvent(customEvent);
+        const hasAddress = await checkHasAddressPromise();
+        console.log("hasAddress", hasAddress);
+        if (hasAddress.hasAddress) {
+            setUnsubscribeConfirmationShow(true);
+        } else {
+            NotificationManager.error(`The address ${walletAddress} not found in the ICONEX`)
+        }
+
+    }
+
     return (
-        <Container>
-            unsubscribe
+        <div style = {{display: 'flex', flexDirection: 'column', justifyContent: 'space-between', height: '100%'}}>
+        <Container style = {{color: '#262626'}}>
+            <div className = {styles.title}>Email Notification Opt Out</div>
+            <div className = {styles.confirmationMessage}>Are you sure you want to opt out of the email list for the wallet address <span style = {{fontWeight: '600'}}>{walletAddress}</span></div>
+            <div className = {styles.optOutButtonContainer}>
+                <Button variant = "primary" className = {styles.optOutButton} onClick = {onClickOptOut}>Yes, I would like to opt out.</Button>
+            </div>
         </Container>
+        <Footer console/>
+
+        <ConfirmationModal
+                show={unsubscribeConfirmationShow}
+                onHide={() => setUnsubscribeConfirmationShow(false)}
+                heading={'Email Notification Opt Out Confirmation'}
+                size="md"
+                onConfirm={() => {
+                    // submitProgressReport(
+                    //     {
+                    //         progressReport
+                    //     }
+                    // )
+                }} >
+                {
+                    <>
+                        <div>Are you sure you want to opt out of the email list?</div>
+                    </>
+                }
+
+        </ConfirmationModal>
+        </div>
     );
 }
 
-export default UnsubscribePage;
+const mapStateToProps = state => (
+    {
+        hasAddress: state.account.hasAddress
+    }
+)
+
+export default connect(mapStateToProps)(UnsubscribePage);
