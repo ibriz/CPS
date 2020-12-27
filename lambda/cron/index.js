3ef86fe39d308e04409c033bfb5eaece089ba174
Bibek <bibek.koirala@hbgdesignlab.se>
2020-12-27 17:56:48 +0545

    Indentation fixed

--- lambda/cron/index.js
+++ lambda/cron/index.js
@@ -1,35 +1,34 @@
 const cron = require('./src/cron');
 
 exports.handler = async (_event) => {
+	try {
+		const cron_actions = await cron.execute();
 
-    try {
-        const cron_actions = await cron.execute();
+		console.log(cron_actions);
 
-        console.log(cron_actions);
+		return Promise.all(cron_actions).then(() => {
+			console.log('==============Invoke Successful==========');
+			Promise.resolve({
+				statusCode: 200,
+				headers: {
+					'Access-Control-Allow-Origin': '*'
+				},
+				body: 'Email sucessfully sent. Check logs for more details.'
+			});
+		});
+	} catch (err) {
 
-        return Promise.all(cron_actions).then(() => {
-            console.log('==============Invoke Successful==========');
-            Promise.resolve({
-                statusCode: 200,
-                headers: {
-                    'Access-Control-Allow-Origin': '*'
-                },
-                body: 'Email sucessfully sent. Check logs for more details.'
-            });
-        });
-    } catch (err) {
-
-        console.log(err);
-        return {
-            statusCode: err.statusCode ? err.statusCode : 500,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Headers': '*'
-            },
-            body: JSON.stringify({
-                error: err.name ? err.name : 'Exception',
-                message: err.message ? err.message : 'Unknown error',
-            }),
-        };
-    }
+		console.log(err);
+		return {
+			statusCode: err.statusCode ? err.statusCode : 500,
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Headers': '*'
+			},
+			body: JSON.stringify({
+				error: err.name ? err.name : 'Exception',
+				message: err.message ? err.message : 'Unknown error',
+			}),
+		};
+	}
 };
--- lambda/cron/src/mail.js
+++ lambda/cron/src/mail.js
@@ -1,44 +1,43 @@
 const AWS = require('aws-sdk');
-const fs = require('fs');
 // SES initialize
 const SES = new AWS.SES();
 
 const emailFrom = process.env.EMAIL_FROM;
 
 async function send_bulk_email(template, userDetails, subject, default_params) {
-    console.log(userDetails);
-    if (userDetails.length > 0) {
-        let params = {
-            Source: emailFrom,
-            Template: template,
-            Destinations: [],
-            DefaultTemplateData: `{
+	console.log(userDetails);
+	if (userDetails.length > 0) {
+		let params = {
+			Source: emailFrom,
+			Template: template,
+			Destinations: [],
+			DefaultTemplateData: `{
                 \"Subject\": \"${subject}\",
                 \"default_params\":\"${default_params}\",
                 \"frontend_url\":\"${process.env.FRONTEND_URL}\"
             }`
-        }
+		}
 
-        for (const user of userDetails) {
-            params.Destinations.push({
-                Destination: {
-                    ToAddresses: [user.email]
-                },
-                ReplacementTemplateData: user.replacementTemplateData
-            })
-        }
+		for (const user of userDetails) {
+			params.Destinations.push({
+				Destination: {
+					ToAddresses: [user.email]
+				},
+				ReplacementTemplateData: user.replacementTemplateData
+			})
+		}
 
-        console.log('Params for sending email' + JSON.stringify(params));
+		console.log('Params for sending email' + JSON.stringify(params));
 
-        try {
-            await SES.sendBulkTemplatedEmail(params).promise();
-            console.log('Email successfully sent params: ' + JSON.stringify(params));
-        } catch (e) {
-            console.error(e);
-            console.error('Email sending Failed! ' + JSON.stringify(params));
-            throw new Error(e);
-        }
-    }
+		try {
+			await SES.sendBulkTemplatedEmail(params).promise();
+			console.log('Email successfully sent params: ' + JSON.stringify(params));
+		} catch (e) {
+			console.error(e);
+			console.error('Email sending Failed! ' + JSON.stringify(params));
+			throw new Error(e);
+		}
+	}
 }
 
 module.exports = { send_bulk_email }
