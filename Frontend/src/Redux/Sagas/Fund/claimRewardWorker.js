671391b06acb45d9ab909a549e2d0a02e80db6b1
Tejasvi Raj Pant <tejpant@gmail.com>
2020-12-30 14:19:10 +0545

    Fixed bugs for fetching expected fund and changed claim_reward to cps treasury score

--- Frontend/src/Redux/Sagas/Fund/claimRewardWorker.js
+++ Frontend/src/Redux/Sagas/Fund/claimRewardWorker.js
@@ -1,10 +1,18 @@
 import { sendTransaction } from 'Redux/ICON/utils';
+import { select } from 'redux-saga/effects';
 
 function* claimRewardWorker({ payload }) {
 
+
+    const getCPSTreasuryScoreAddress = (state) => state.fund.cpsTreasuryScoreAddress
+    const cpsTreasuryScoreAddress = yield select(getCPSTreasuryScoreAddress);
+
     sendTransaction({
         method: 'claim_reward',
-    }
+        scoreAddress: cpsTreasuryScoreAddress
+
+        },
+
     )
 
 }
