c8bf5ab3d9c8a5b1f96f79279f8dbcf024d214ad
Tejasvi Raj Pant <tejpant@gmail.com>
2021-01-20 21:31:54 +0545

    Solved image upload problem

--- Frontend/src/Redux/Constants/index.js
+++ Frontend/src/Redux/Constants/index.js
@@ -1,5 +1,5 @@
-export const BASE_URL = 'https://u7hj3y857d.execute-api.us-east-2.amazonaws.com/cps';
-// export const BASE_URL = 'https://65ca82rpg0.execute-api.us-west-2.amazonaws.com/stage';
+// export const BASE_URL = 'https://u7hj3y857d.execute-api.us-east-2.amazonaws.com/cps';
+export const BASE_URL = 'https://65ca82rpg0.execute-api.us-west-2.amazonaws.com/stage';
 export const IPFS_URL = 'https://gateway.ipfs.io/ipfs';
 
 export const PROPOSAL_ADD_URL = 'proposals';
@@ -8,7 +8,7 @@ export const PROGRESS_REPORT_ADD_URL = 'proposals';
 export const RESEND_EMAIL_VERIFICATION = 'user/verify';
 export const USER_PROMPT = 'user/prompt';
 
-export const IMAGE_UPLOAD_URL = '/proposals/image';
+export const IMAGE_UPLOAD_URL = 'proposals/image';
 
 export const ADD_PROPOSAL_DRAFT_URL = 'draft';
 export const ADD_PROGRESS_REPORT_DRAFT_URL = 'draft';
