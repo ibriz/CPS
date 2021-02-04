41ca2234b6a368ff8bce4d9cf6b3b5c100049656
Tejasvi Raj Pant <tejpant@gmail.com>
2021-02-04 16:11:47 +0545

    Form validation on sponsor vote submission

--- Frontend/src/Redux/Sagas/Proposal/fetchSponsorRequestsListWorker.js
+++ Frontend/src/Redux/Sagas/Proposal/fetchSponsorRequestsListWorker.js
@@ -14,30 +14,30 @@ const proposalListStatusMapping = {
 
 function* fetchSponsorRequestsListWorker({payload}) {
   try {
-    const response = yield call(callKeyStoreWallet, {
-      method: 'get_sponsors_requests',
-      params: {_status: proposalListStatusMapping[payload.status],
-        _sponsor_address: payload.walletAddress,
-      _end_index: `${(payload.pageNumber * 10) - 1}`,
-      _start_index: `${(payload.pageNumber * 10) - 10}`,
+//     const response = yield call(callKeyStoreWallet, {
+//       method: 'get_sponsors_requests',
+//       params: {_status: proposalListStatusMapping[payload.status],
+//         _sponsor_address: payload.walletAddress,
+//       _end_index: `${(payload.pageNumber * 10) - 1}`,
+//       _start_index: `${(payload.pageNumber * 10) - 10}`,
     
-    }
-});
+//     }
+// });
 
-    // const response = {
-    //   data: Array(10).fill(0).map((_, index) => (  {
-    //         _status:"_sponsor_pending",
-    //         _proposal_title :`${payload.status} ${payload.pageNumber}${index} New Sponsor Request`,
-    //         _contributor_address : (index % 2 === 0) ? "hx177b10efe3018961a405cc7c20ee811c552831a5": "hxfd114a60eefa8e2c3de2d00dc5e41b1a0c7e8931",
-    //         budget : 10000,
-    //         _timestamp : 1600872145290985,
-    //         _ipfs_hash: "bafybeih63y3v5goi3if4e3yrwsjnc2lv7uiesui2cuamfvvgebki5ewo2e",
-    //           _ipfs_key: "proposal338e7daa-867e-4ea1-995b-b0e118b0036e"
-    //     }
-    //     )),
-    //     _total_items: 143
+    const response = {
+      data: Array(10).fill(0).map((_, index) => (  {
+            status:"_sponsor_pending",
+            project_title :`${payload.status} ${payload.pageNumber}${index} New Sponsor Request`,
+            contributor_address : (index % 2 === 0) ? "hx177b10efe3018961a405cc7c20ee811c552831a5": "hxfd114a60eefa8e2c3de2d00dc5e41b1a0c7e8931",
+            total_budget : 10000,
+            timestamp : 1600872145290985,
+            ipfs_hash: "bafybeih63y3v5goi3if4e3yrwsjnc2lv7uiesui2cuamfvvgebki5ewo2e",
+              ipfs_key: "proposal338e7daa-867e-4ea1-995b-b0e118b0036e"
+        }
+        )),
+        _total_items: 143
       
-    // }
+    }
     yield put(fetchSponsorRequestsListSuccess(
       {
         response,
