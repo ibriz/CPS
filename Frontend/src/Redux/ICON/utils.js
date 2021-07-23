73baae7ed3ffdeded7cabaa8df2ff7326e57c04e
Shailesh <shaileshkandel123@gmail.com>
2021-07-23 18:33:29 +0545

    CPS mainnet info

--- Frontend/src/Redux/ICON/utils.js
+++ Frontend/src/Redux/ICON/utils.js
@@ -12,14 +12,14 @@ import frontEndWallet from './FrontEndWallet';
 // var CPSScore = 'cx00c1e2d9b009fca69002c53c1ce3ed377708381e';
 // var CPSScore = 'cx6bb0e6683dd326165d42289c12b6bd0eaa596cc9';
 // var CPSScore = 'cx9f4ab72f854d3ccdc59aa6f2c3e2215dd62e879f';
-// export const CPSScore = 'cx9f4ab72f854d3ccdc59aa6f2c3e2215dd62e879f';
-export const CPSScore = 'cx7b98401aa6578296abd311b4cb70e90812e9ebae';
+export const CPSScore = 'cx9f4ab72f854d3ccdc59aa6f2c3e2215dd62e879f';
+// export const CPSScore = 'cx7b98401aa6578296abd311b4cb70e90812e9ebae';
 
-var nid = 3;
-// export const provider = new HttpProvider('https://ctz.solidwallet.io/api/v3');
-export const provider = new HttpProvider(
-  'https://bicon.net.solidwallet.io/api/v3',
-);
+var nid = 1;
+export const provider = new HttpProvider('https://ctz.solidwallet.io/api/v3');
+// export const provider = new HttpProvider(
+//   'https://bicon.net.solidwallet.io/api/v3',
+// );
 
 export const iconService = new IconService(provider);
 export const trackerURL = 'https://tracker.icon.foundation/address';
