d5a86eb2adbefca659a8eea216444b991cef9dc6
sharma66mahesh <gauravpudel1990@gmail.com>
2021-07-26 16:29:54 +0545

    Comment out email notifications not triggered when changing period

--- lambda/cron/src/cron.js
+++ lambda/cron/src/cron.js
@@ -209,7 +209,6 @@ async function execute() {
 			console.log('Updating period...');
 
 			await score.update_period();
-			period_triggered = true;
 			await sleep(2000);	// sleep for 2 secs
 			present_period = await score.period_check();
 			console.log('Changed period to: ' + present_period['period_name']);
@@ -219,6 +218,8 @@ async function execute() {
 				present_period = await score.period_check();
 				console.log('Changed period to: ' + present_period['period_name']);
 			}
+			
+			period_triggered = true;
 
 			const periodEndingDate = new Date();
 			periodEndingDate.setDate(periodEndingDate.getDate() + 15);
@@ -303,53 +304,53 @@ async function execute() {
 		if (present_period.period_name === PERIOD_MAPPINGS.APPLICATION_PERIOD && user_details_list.length > 0) {
 			console.log('=====================Notifications for Application Period=======================');
 
-			if (parseInt(present_period.remaining_time) <= DAY) {
-				const progress_report_reminder_before_one_day_async = score.progress_report_reminder_before_one_day(user_details_list).then(async (contributor_notification_list) => {
-					if (contributor_notification_list !== undefined && contributor_notification_list.length > 0) {
-						console.log('contributor_notification_list' + contributor_notification_list)
-						console.log('Sending emails to ' + contributor_notification_list.length + ' contributors');
-						await mail.send_bulk_email('contributor-reminder',
-							contributor_notification_list,
-							'One day remaining for progress report | ICON CPS',
-							`,\"time\": \"24 hours\", \"type\": \"Progress Report\"`);
-					} else {
-						console.log('No user to send notification: progress_report_reminder_before_one_day_async')
-					}
-				})
+			// if (parseInt(present_period.remaining_time) <= DAY) {
+			// 	const progress_report_reminder_before_one_day_async = score.progress_report_reminder_before_one_day(user_details_list).then(async (contributor_notification_list) => {
+			// 		if (contributor_notification_list !== undefined && contributor_notification_list.length > 0) {
+			// 			console.log('contributor_notification_list' + contributor_notification_list)
+			// 			console.log('Sending emails to ' + contributor_notification_list.length + ' contributors');
+			// 			await mail.send_bulk_email('contributor-reminder',
+			// 				contributor_notification_list,
+			// 				'One day remaining for progress report | ICON CPS',
+			// 				`,\"time\": \"24 hours\", \"type\": \"Progress Report\"`);
+			// 		} else {
+			// 			console.log('No user to send notification: progress_report_reminder_before_one_day_async')
+			// 		}
+			// 	})
 
-				actions.push(progress_report_reminder_before_one_day_async);
-			} else if (parseInt(period.remaining_time) <= 7 * DAY && parseInt(period.remaining_time) >= 6 * DAY) {
-				const progress_report_reminder_before_one_week = score.progress_report_reminder_before_one_week(user_details_list).then(async (contributor_notification_list) => {
-					if (contributor_notification_list !== undefined && contributor_notification_list.length > 0) {
-						console.log('contributor_notification_list' + contributor_notification_list)
-						console.log('Sending emails to ' + contributor_notification_list.length + ' contributors');
-						await mail.send_bulk_email('contributor-reminder',
-							contributor_notification_list,
-							'One week remaining for progress report | ICON CPS',
-							`,\"time\": \"one week\", \"type\": \"Progress Report\"`);
-					} else {
-						console.log('No user to send notification: progress_report_reminder_before_one_week')
-					}
-				})
+			// 	actions.push(progress_report_reminder_before_one_day_async);
+			// } else if (parseInt(period.remaining_time) <= 7 * DAY && parseInt(period.remaining_time) >= 6 * DAY) {
+			// 	const progress_report_reminder_before_one_week = score.progress_report_reminder_before_one_week(user_details_list).then(async (contributor_notification_list) => {
+			// 		if (contributor_notification_list !== undefined && contributor_notification_list.length > 0) {
+			// 			console.log('contributor_notification_list' + contributor_notification_list)
+			// 			console.log('Sending emails to ' + contributor_notification_list.length + ' contributors');
+			// 			await mail.send_bulk_email('contributor-reminder',
+			// 				contributor_notification_list,
+			// 				'One week remaining for progress report | ICON CPS',
+			// 				`,\"time\": \"one week\", \"type\": \"Progress Report\"`);
+			// 		} else {
+			// 			console.log('No user to send notification: progress_report_reminder_before_one_week')
+			// 		}
+			// 	})
 
-				actions.push(progress_report_reminder_before_one_week);
-			} else {
-				console.log('No reminders sent to users');
-			}
+			// 	actions.push(progress_report_reminder_before_one_week);
+			// } else {
+			// 	console.log('No reminders sent to users');
+			// }
 
-			const sponsorship_accepted_notification_async = score.sponsorship_accepted_notification(user_details_list).then(async (contributor_notification_list) => {
-				if (contributor_notification_list !== undefined && contributor_notification_list.length > 0) {
-					console.log('contributor_notification_list' + contributor_notification_list)
-					console.log('Sending emails to ' + contributor_notification_list.length + ' contributors');
-					await mail.send_bulk_email('sponsorship-accepted',
-						contributor_notification_list,
-						'Sponsorship Request Accepted | ICON CPS');
-				} else {
-					console.log('No user to send notification: sponsorship_accepted_notification_async')
-				}
-			})
+			// const sponsorship_accepted_notification_async = score.sponsorship_accepted_notification(user_details_list).then(async (contributor_notification_list) => {
+			// 	if (contributor_notification_list !== undefined && contributor_notification_list.length > 0) {
+			// 		console.log('contributor_notification_list' + contributor_notification_list)
+			// 		console.log('Sending emails to ' + contributor_notification_list.length + ' contributors');
+			// 		await mail.send_bulk_email('sponsorship-accepted',
+			// 			contributor_notification_list,
+			// 			'Sponsorship Request Accepted | ICON CPS');
+			// 	} else {
+			// 		console.log('No user to send notification: sponsorship_accepted_notification_async')
+			// 	}
+			// })
 
-			actions.push(sponsorship_accepted_notification_async);
+			// actions.push(sponsorship_accepted_notification_async);
 
 			if (period_triggered) {
 				const proposal_accepted_notification_async = score.proposal_accepted_notification(user_details_list).then(async (contributor_notification_list) => {
@@ -395,65 +396,65 @@ async function execute() {
 			}
 		} else if (present_period.period_name === 'Voting Period' && preps_list.length > 0) {
 			console.log('=====================Notifications for Voting Period=======================');
-			if (parseInt(present_period.remaining_time) <= DAY) {
-				const voting_reminder_before_one_day_proposal_async = score.voting_reminder_before_one_day(preps_list, 'Proposal').then(async (preps_notification_list) => {
-					if (preps_notification_list !== undefined && preps_notification_list.length > 0) {
-						console.log('preps_notification_list' + preps_notification_list)
-						console.log('Sending emails to ' + preps_notification_list.length + ' preps');
-						await mail.send_bulk_email('prep-day-reminder',
-							preps_notification_list,
-							'One day remaining for voting | ICON CPS',
-							`,\"icx\": \"${process.env.ICX_PENALTY}\"`);
-					} else {
-						console.log('No user to send notification: voting_reminder_before_one_day_proposal_async')
-					}
-				})
+			// if (parseInt(present_period.remaining_time) <= DAY) {
+			// 	const voting_reminder_before_one_day_proposal_async = score.voting_reminder_before_one_day(preps_list, 'Proposal').then(async (preps_notification_list) => {
+			// 		if (preps_notification_list !== undefined && preps_notification_list.length > 0) {
+			// 			console.log('preps_notification_list' + preps_notification_list)
+			// 			console.log('Sending emails to ' + preps_notification_list.length + ' preps');
+			// 			await mail.send_bulk_email('prep-day-reminder',
+			// 				preps_notification_list,
+			// 				'One day remaining for voting | ICON CPS',
+			// 				`,\"icx\": \"${process.env.ICX_PENALTY}\"`);
+			// 		} else {
+			// 			console.log('No user to send notification: voting_reminder_before_one_day_proposal_async')
+			// 		}
+			// 	})
 
-				const voting_reminder_before_one_day_progress_report_async = score.voting_reminder_before_one_day(preps_list, 'Progress Report').then(async (preps_notification_list) => {
-					if (preps_notification_list !== undefined && preps_notification_list.length > 0) {
-						console.log('preps_notification_list' + preps_notification_list)
-						console.log('Sending emails to ' + preps_notification_list.length + ' preps');
-						await mail.send_bulk_email('prep-day-reminder',
-							preps_notification_list,
-							'One day remaining for voting | ICON CPS',
-							`,\"icx\": \"${process.env.ICX_PENALTY}\"`);
-					} else {
-						console.log('No user to send notification: voting_reminder_before_one_day_progress_report_async')
-					}
-				})
+			// 	const voting_reminder_before_one_day_progress_report_async = score.voting_reminder_before_one_day(preps_list, 'Progress Report').then(async (preps_notification_list) => {
+			// 		if (preps_notification_list !== undefined && preps_notification_list.length > 0) {
+			// 			console.log('preps_notification_list' + preps_notification_list)
+			// 			console.log('Sending emails to ' + preps_notification_list.length + ' preps');
+			// 			await mail.send_bulk_email('prep-day-reminder',
+			// 				preps_notification_list,
+			// 				'One day remaining for voting | ICON CPS',
+			// 				`,\"icx\": \"${process.env.ICX_PENALTY}\"`);
+			// 		} else {
+			// 			console.log('No user to send notification: voting_reminder_before_one_day_progress_report_async')
+			// 		}
+			// 	})
 
-				actions.push(voting_reminder_before_one_day_proposal_async, voting_reminder_before_one_day_progress_report_async);
-			} else if (parseInt(period.remaining_time) <= 7 * DAY && parseInt(period.remaining_time) >= 6 * DAY) {
-				const progress_report_reminder_before_one_week_proposal_async = score.voting_reminder_before_one_week(preps_list, 'Proposal').then(async (preps_notification_list) => {
-					if (preps_notification_list !== undefined && preps_notification_list.length > 0) {
-						console.log('preps_notification_list' + preps_notification_list)
-						console.log('Sending emails to ' + preps_notification_list.length + ' preps');
-						await mail.send_bulk_email('prep-week-reminder',
-							preps_notification_list,
-							'One week remaining for voting | ICON CPS');
-					} else {
-						console.log('No user to send notification: progress_report_reminder_before_one_week_proposal_async')
-					}
-				})
+			// 	actions.push(voting_reminder_before_one_day_proposal_async, voting_reminder_before_one_day_progress_report_async);
+			// } else if (parseInt(period.remaining_time) <= 7 * DAY && parseInt(period.remaining_time) >= 6 * DAY) {
+			// 	const progress_report_reminder_before_one_week_proposal_async = score.voting_reminder_before_one_week(preps_list, 'Proposal').then(async (preps_notification_list) => {
+			// 		if (preps_notification_list !== undefined && preps_notification_list.length > 0) {
+			// 			console.log('preps_notification_list' + preps_notification_list)
+			// 			console.log('Sending emails to ' + preps_notification_list.length + ' preps');
+			// 			await mail.send_bulk_email('prep-week-reminder',
+			// 				preps_notification_list,
+			// 				'One week remaining for voting | ICON CPS');
+			// 		} else {
+			// 			console.log('No user to send notification: progress_report_reminder_before_one_week_proposal_async')
+			// 		}
+			// 	})
 
-				const progress_report_reminder_before_one_week_progress_report_async = score.voting_reminder_before_one_week(preps_list, 'Progress Report').then(async (preps_notification_list) => {
-					if (preps_notification_list !== undefined && preps_notification_list.length > 0) {
-						console.log('preps_notification_list' + preps_notification_list)
-						console.log('Sending emails to ' + preps_notification_list.length + ' preps');
-						await mail.send_bulk_email('prep-week-reminder',
-							preps_notification_list,
-							'One week remaining for voting | ICON CPS');
-					} else {
-						console.log('No user to send notification: progress_report_reminder_before_one_week_progress_report_async')
-					}
-				})
+			// 	const progress_report_reminder_before_one_week_progress_report_async = score.voting_reminder_before_one_week(preps_list, 'Progress Report').then(async (preps_notification_list) => {
+			// 		if (preps_notification_list !== undefined && preps_notification_list.length > 0) {
+			// 			console.log('preps_notification_list' + preps_notification_list)
+			// 			console.log('Sending emails to ' + preps_notification_list.length + ' preps');
+			// 			await mail.send_bulk_email('prep-week-reminder',
+			// 				preps_notification_list,
+			// 				'One week remaining for voting | ICON CPS');
+			// 		} else {
+			// 			console.log('No user to send notification: progress_report_reminder_before_one_week_progress_report_async')
+			// 		}
+			// 	})
 
-				actions.push(progress_report_reminder_before_one_week_proposal_async, progress_report_reminder_before_one_week_progress_report_async);
-			} else {
-				console.log('No reminders sent to users');
-				console.log('No of notification enabled preps: ' + preps_list.length);
-				console.log('No of notification enabled contributors' + user_details_list.length);
-			}
+			// 	actions.push(progress_report_reminder_before_one_week_proposal_async, progress_report_reminder_before_one_week_progress_report_async);
+			// } else {
+			// 	console.log('No reminders sent to users');
+			// 	console.log('No of notification enabled preps: ' + preps_list.length);
+			// 	console.log('No of notification enabled contributors' + user_details_list.length);
+			// }
 		}
 	} catch (error) {
 		throw new Error(error);
