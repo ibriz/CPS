912e0a5912caea5398de06815e075f3b3bd9ee68
sabal-ibriz <sabal.kc@ibriz.ai>
2022-08-18 19:48:08 +0545

    Fix #37: Updated grant process

--- Frontend/src/Containers/LandingPage/grantProcess.js
+++ Frontend/src/Containers/LandingPage/grantProcess.js
@@ -1,59 +1,127 @@
 import React from 'react';
 import { Waypoint } from 'react-waypoint';
+import { Link } from 'react-router-dom';
 
-const GrantProcess = (props) => {
+const GrantProcess = props => {
+  const { grantProcessRef } = props;
 
-    const { grantProcessRef } = props;
+  return (
+    <Waypoint
+      onEnter={() => {
+        // setActiveTabCenter('grantProcess')
+      }}
+      onLeave={() => {
+        // setActiveTabCenter('')
+      }}
+    >
+      <div ref={grantProcessRef} className='landingPage__GrantProcess'>
+        <div className='grantProcessContainer scrollSection' id='grantProcess'>
+          <h1>Grant Process</h1>
+          <p>
+            Here’s how the CPS Grants Program process works, from application to
+            finish line{' '}
+          </p>
+          <div className='process'>
+            <ul>
+              <li className='current'>
+                <h3>Familiarize yourself with the CPS</h3>
+                <p>
+                  Read through the{' '}
+                  <a
+                    href='https://medium.com/iconosphere-io/how-to-use-the-contribution-proposal-system-1efe714c9182'
+                    target='_blank'
+                  >
+                    “Getting Started” guide{' '}
+                  </a>
+                  ,{' '}
+                  <a
+                    href='https://github.com/icon-community/CPS/wiki/Applicant-Flow'
+                    target='_blank'
+                  >
+                    Applicant Flow{' '}
+                  </a>
+                  ,{' '}
+                  <a
+                    href='https://github.com/icon-community/CPS/wiki/Application-Templates'
+                    target='_blank'
+                  >
+                    Application Templates
+                  </a>{' '}
+                  and{' '}
+                  <a
+                    href='https://github.com/icon-community/CPS/wiki/Project-Activity-Flow-States'
+                    target='_blank'
+                  >
+                    Project Activity Flow States{' '}
+                  </a>{' '}
+                  to familiarize yourself with the CPS and the application
+                  process in detail.
+                </p>
+              </li>
+              <li className='current'>
+                <h3>Log in/Create an account in the CPS App</h3>
+                <p>
+                  Explore the <Link to='/dashboard'>CPS Application</Link> and
+                  all the current proposals/active projects being funded.
+                </p>
+              </li>
+              <li className='current'>
+                <h3>Submit your Grant Application</h3>
+                <p>
+                  Using our{' '}
+                  <a
+                    href='https://github.com/icon-project/grants-program/blob/main/templates/yymmdd-project-proposal.md'
+                    target='_blank'
+                  >
+                    standardized application form
+                  </a>
+                  , provide the details for your desired project and find a
+                  Validator to sponsor your idea.
+                </p>
+              </li>
+              <li className='current'>
+                <h3>Evaluation by Validators</h3>
+                <p>
+                  During the “Voting Period”, Validators will vote on your
+                  application and either “Approve” or “Deny” your concept.
+                  Improve your chances in this stage by answering questions and
+                  advocating for your idea.
+                </p>
+              </li>
+              <li className='current '>
+                <h3>Proposal decision</h3>
 
-    return (
-        <Waypoint
-            onEnter={() => {
-                // setActiveTabCenter('grantProcess')
-            }}
-            onLeave={() => {
-                // setActiveTabCenter('')
-            }}
-
-        >
-
-            <div ref={grantProcessRef} className="landingPage__GrantProcess">
-                <div className="grantProcessContainer scrollSection" id="grantProcess">
-                    <h1>Grant Process</h1>
-                    <p>Here’s how the CPS Grants Program process works, from application to finish line </p>
-                    <div className="process">
-                        <ul>
-                            <li className="current"><h3>Read through “Start Here” guide</h3>
-                                <p>Read through the “Start Here” guide on the Console to familiarize yourself with the CPS and the application process in detail.</p></li>
-                            <li className="current">
-                                <h3>Log in/Create an account in the CPS Console</h3>
-                                <p>Explore the CPS Console and all the current proposals/active projects being funded.
-                                </p>
-                            </li>
-                            <li className="current"><h3>Submit your Grant Application
-                            </h3>
-                                <p>Using our standardized application form, provide the details for your desired project and find a P-Rep to sponsor your idea.
-                                </p></li>
-                            <li className="current">
-                                <h3>Evaluation by P-Reps</h3>
-                                <p>During the “Voting Period”, P-Reps will vote on your application and either “Approve” or “Deny” your concept. Improve your chances in this stage by answering questions and advocating for your idea.
-                                </p>
-                            </li>
-                            <li className="current "><h3>Proposal decision</h3>
-
-                                <p>
-                                    If you project is accepted during the "Voting Period", you will have 15 days (the length of the next application period), to submit a "Progress Report". Your Progress Report is then voted on during the next "Voting Period", and if accepted you will receive your first milestone of funds.
-                                </p>
-                            </li>
-                            <li className="current"><h3>Build!
-                            </h3>
-                                <p>Congratulations! As said in the "Acceptance" stage, you now can build your project, and after submitting progress reports that are accepted, you will receive funding for your hard work. After you complete your project, it's time to submit a new idea and continue to grow the ICON ecosystem!
-                                </p></li>
-                        </ul>
-                    </div>
-                </div>
-            </div >
-        </Waypoint>
-    )
-}
+                <p>
+                  If your project is accepted during the "Voting Period", you
+                  will have 15 days (the length of the next application period),
+                  to submit a{' '}
+                  <a
+                    href='https://github.com/icon-project/grants-program/blob/main/templates/yymmdd-project-progress-report.md'
+                    target='_blank'
+                  >
+                    "Progress Report"{' '}
+                  </a>
+                  . Your Progress Report is then voted on during the next
+                  "Voting Period", and if accepted you will receive your first
+                  milestone of funds.
+                </p>
+              </li>
+              <li className='current'>
+                <h3>Build</h3>
+                <p>
+                  Congratulations! As said in the "Acceptance" stage, you can
+                  now build your project, and after submitting progress reports
+                  that are accepted, you will receive funding for your hard
+                  work. After you complete your project, it's time to submit a
+                  new idea and continue to grow the ICON ecosystem!
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </Waypoint>
+  );
+};
 
 export default GrantProcess;
