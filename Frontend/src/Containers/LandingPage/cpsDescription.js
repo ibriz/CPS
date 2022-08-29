11d4f886efa235873afc35308ee45980c129c5a1
sabal-ibriz <sabal.kc@ibriz.ai>
2022-08-29 19:59:38 +0545

    Fix #45: Updated get in touch link to discord

--- Frontend/src/Containers/LandingPage/cpsDescription.js
+++ Frontend/src/Containers/LandingPage/cpsDescription.js
@@ -6,40 +6,58 @@ import { Link, Redirect } from 'react-router-dom';
 import Summary from './summary';
 import { Waypoint } from 'react-waypoint';
 
-const CPSDescription = (props) => {
-    const { walletAddress, onClickLogin, setActiveTabCenter, createProposalRef } = props;
-    const [clickedProposal, setClickedProposal] = useState(false);
+const CPSDescription = props => {
+  const { walletAddress, onClickLogin, setActiveTabCenter, createProposalRef } =
+    props;
+  const [clickedProposal, setClickedProposal] = useState(false);
 
-    if (clickedProposal && walletAddress) {
-        return <Redirect to="/newProposal" />
-    }
-    return (
-        <Waypoint
-            onEnter={() => {
-                // setActiveTabCenter('description')
-            }}
-            onLeave={() => {
-                // setActiveTabCenter('')
-            }}
-        >
-            <div ref={createProposalRef} className="landingPage__Description">
-
-                <div className="descriptionLeft scrollSection" id="description">
-                    <h3>Launch your project on </h3>
-                    <img src={iconBlackImg}></img>
-                    <p>The Contribution Proposal System (CPS) is a decentralized grant program that supports developers and teams wanting to build on the ICON blockchain.</p>
-                    <div className="buttons">
-                        <p onClick={() => {
-                            setClickedProposal(true);
-                            if (!walletAddress) {
-                                onClickLogin();
-                            }
-                        }} className="active">Create a proposal</p>
-                        <p><a style={{ color: '#27AAB9', textDecoration: 'none' }} href="mailto:tj@icon.foundation" target="_blank">Get in touch</a></p>
-                    </div>
-                </div>
-            </div>
-        </Waypoint>)
-}
+  if (clickedProposal && walletAddress) {
+    return <Redirect to='/newProposal' />;
+  }
+  return (
+    <Waypoint
+      onEnter={() => {
+        // setActiveTabCenter('description')
+      }}
+      onLeave={() => {
+        // setActiveTabCenter('')
+      }}
+    >
+      <div ref={createProposalRef} className='landingPage__Description'>
+        <div className='descriptionLeft scrollSection' id='description'>
+          <h3>Launch your project on </h3>
+          <img src={iconBlackImg}></img>
+          <p>
+            The Contribution Proposal System (CPS) is a decentralized grant
+            program that supports developers and teams wanting to build on the
+            ICON blockchain.
+          </p>
+          <div className='buttons'>
+            <p
+              onClick={() => {
+                setClickedProposal(true);
+                if (!walletAddress) {
+                  onClickLogin();
+                }
+              }}
+              className='active'
+            >
+              Create a proposal
+            </p>
+            <p>
+              <a
+                style={{ color: '#27AAB9', textDecoration: 'none' }}
+                href='https://discord.gg/4vpFeYams4'
+                target='_blank'
+              >
+                Get in touch
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </Waypoint>
+  );
+};
 
 export default CPSDescription;
