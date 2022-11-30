b8555bcf51d2664ce4ab2f4cbc5ff26bbc661dc6
sdpbhandari <bhandari.sudeep68@gmail.com>
2022-12-01 00:03:49 +0545

    fix proposals orders on Sponsored Projects List/My Proposal List

--- Frontend/src/Components/SponsorRequestsCard/sortOrderProposalStates.js
+++ Frontend/src/Components/SponsorRequestsCard/sortOrderProposalStates.js
@@ -1,5 +1,10 @@
 export default {
-  Active: 1,
-  Voting: 2,
-  Completed: 3,
+  Voting: 1,
+  Active: 2,
+  Pending: 3,
+  Paused: 4,
+  Completed: 5,
+  Rejected: 6,
+  Disqualified: 7,
+  Draft: 8,
 };
