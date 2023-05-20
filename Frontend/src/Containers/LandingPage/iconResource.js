98bc30ee51fcbdf6fec73e1f90ff50ba62b52366
PaulRouge <65511035+paulrouge@users.noreply.github.com>
2023-05-20 14:08:27 +0200

    Update iconResource.js
    Removed non-active links.

--- Frontend/src/Containers/LandingPage/iconResource.js
+++ Frontend/src/Containers/LandingPage/iconResource.js
@@ -14,13 +14,14 @@ const IconResource = (props) => {
             <a target="_blank" href="https://www.icondev.io/">
                 <img src={iconDevPortalImg} />
             </a>
+            {/* 
             <a target="_blank" href="https://docs.opendevicon.io/">
                 <img src={iconDevIconImg} />
             </a>
             <a target="_blank" href="https://tsf.opendevicon.io/">
                 <img src={tokenScoreFactoryImg} />
             </a>
-            {/* <a target="_blank" href="https://iconweb.studio/">
+            <a target="_blank" href="https://iconweb.studio/">
                 <img src={iconWebStudioImg} />
             </a> */}
         </div>
